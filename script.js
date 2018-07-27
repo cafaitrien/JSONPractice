3e7e024c2ebbbe961d19747d2cd810d3448f7211
Michael Cobb <cobbmj@uw.edu>
2018-07-26 20:47:01 -0700

    basic validation added

--- script.js
+++ script.js
@@ -25,6 +25,7 @@ neighborhood = [
 ];
 
 let neighborType = ["is Awesome", "is great", "is fantastic", "very Disneyesque", "is Terrible", "talks A LOT", "yells at people walking on the sidewalk"];
+// TODO Randomly name our neighbors from potentially from external site to give us a wide choice of names
 for(let i=0; i<neighborhood.length; i++){
   let container = document.querySelector("div#container");
   let divider = document.createElement("p");
@@ -46,8 +47,45 @@ function build(obj, container){
 
     //append it to the body
     div.appendChild(p)
-
-
   }
   container.appendChild(div);
 }
+
+//New form submission handler
+const form = document.querySelector("form");
+form.addEventListener("submit", function(event){
+  event.preventDefault()
+  console.log("form is being handled!")
+
+  let errorMessages = "";
+  let newRoof = this.roof.value;
+  if(newRoof === "" || newRoof.trim().length === 0){
+    newRoof = "unknown";
+  }
+  let newExterior = this.exterior.value;
+  if(newExterior === "" || newExterior.trim().length === 0){
+    newExterior = "unknown";
+  }
+
+  let newBathrooms = parseFloat(this.bathrooms.value);
+  if(isNaN(newBathrooms)|| newBathrooms > 5 || newBathrooms < 0.5){
+    errorMessages += "Number of bathrooms must be a number between 0.5 and 5";
+  }
+  let newBedrooms = parseInt(this.bedrooms.value);
+  if(isNaN(newBedrooms) || newBedrooms > 5 || newBedrooms < 1){
+    errorMessages += "Number of bedrooms must be a number between 1 and 5";
+  }
+  let newSqft = parseInt(this.sqft.value);
+  if(isNaN(newSqft)){
+    errorMessages += "sqft is required";
+  }
+  let newHasYard = parseInt(this.hasYard.value);
+  if(isNaN(newHasYard)){
+    errorMessages += "Has Yard is required";
+  }
+})
+// validate fields, numbers not negative, are numbers
+// sqft hasYard required
+//rooms <= 5
+
+//build new house and insert in neighborhood if good
